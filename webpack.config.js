12ef5c7669f5ad555241c085791033762aa97dff
metadolism <metadolism@gmail.com>
2023-12-13 02:59:16 +0330

    b

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,40 @@
-const path = require('path');
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-module.exports = {
-  mode: 'development', // or 'production'
-  target: 'node',
-  entry: './index.js',
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: 'MET',
-    libraryTarget: 'umd'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  },
-  node: {
-    global: true,
-    __filename: 'mock',
-    __dirname: 'mock',
-    Buffer: true,
-    process: true,
-  },
-  externals: {
-    mqtt: 'commonjs mqtt'
-  }
+const path = require('path');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+
+const config = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
 };
